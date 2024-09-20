b1e8189abd2f9c2fcf22e1a7451cb13278a754bb
AliAksoyy <alifeyza4017@gmail.com>
2024-09-20 04:53:30 +0300

    contuine

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
+import { useLang } from "../context/LocalizationContext";
 
-const Layout = () => {
+const Layout = ({ drawerModal, setDrawerModal }) => {
+  const dispatch = useDispatch();
+  const { lang, setLang } = useLang();
   return (
-    <div>Layout</div>
-  )
-}
+    <div>
+      <div>Layout</div>
+      <Outlet />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -12,6 +12,41 @@ import AccomplishedResetPassword from "../pages/AccomplishedResetPassword";
 import ResetPassword from "../pages/ResetPassword";
 import LinkExpired from "../pages/LinkExpired";
 
-const AppRouter = () => {};
+const AppRouter = () => {
+  const [drawerModal, setDrawerModal] = useState(false);
+
+  return (
+    <Routes>
+      <Route
+        element={
+          <Layout drawerModal={drawerModal} setDrawerModal={setDrawerModal} />
+        }
+      >
+        <Route path="/" element={<Home drawerModal={drawerModal} />} />
+        <Route
+          path="/mdRender/:id"
+          element={<MarkDownRenderPage drawerModal={drawerModal} />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/news" element={<News drawerModal={drawerModal} />} />
+        <Route
+          path="/news/:id"
+          element={<NewsDetail drawerModal={drawerModal} />}
+        />
+        <Route
+          path="/accomplishedPassword"
+          element={<AccomplishedResetPassword />}
+        />
+        <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route
+          path="/contact"
+          element={<ContactUs drawerModal={drawerModal} />}
+        />
+        <Route path="/linkExpired" element={<LinkExpired />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default AppRouter;

bfaf929b40a4209dfd7706c0d29bafa7f7a7965e
AliAksoyy <alifeyza4017@gmail.com>
2024-09-20 05:23:17 +0300

    added

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,16 +1,62 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useCallback, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Drawer from "../components/Drawer";
 import { useLang } from "../context/LocalizationContext";
+import { useDispatch } from "react-redux";
+import { setLangValues } from "../features/localization/localizationSlice";
+import { getNews } from "../features/news/newsSlice";
 
 const Layout = ({ drawerModal, setDrawerModal }) => {
   const dispatch = useDispatch();
   const { lang, setLang } = useLang();
+
+  useEffect(() => {
+    const getNewsData = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.REACT_APP_API_URL}/public/news.json`
+        );
+        const data = await res.json();
+        dispatch(getNews(data));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNewsData();
+  }, [dispatch]);
+
+  useEffect(() => {
+    setLang(localStorage.getItem("localization") || lang);
+  }, [lang, setLang]);
+
+  const setLanguageData = useCallback(
+    async (lang) => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/localization/${lang}.json`
+        );
+        const data = await response.json();
+        dispatch(setLangValues({ lang, data }));
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    setLanguageData(lang);
+  }, [lang, setLanguageData]);
+
   return (
-    <div>
-      <div>Layout</div>
+    <>
+      <Header drawerModal={drawerModal} setDrawerModal={setDrawerModal} />
+      <Drawer drawerModal={drawerModal} setDrawerModal={setDrawerModal} />
       <Outlet />
-    </div>
+      <Footer />
+    </>
   );
 };
 

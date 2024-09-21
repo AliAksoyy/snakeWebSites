2db85ad546a1b86ec7f38145c7f6e6fde7be8a78
AliAksoyy <alifeyza4017@gmail.com>
2024-09-22 02:36:29 +0300

    telephone finished

--- src/components/Telephone/TabletContainer/index.js
+++ src/components/Telephone/TabletContainer/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styles from "./styles.module.scss";
+import tabletTelephone from "../../../assets/telephoneImg/tabletTelephone.png";
+import StoreButton from "../../StoreButton";
+import { useLang } from "../../../context/LocalizationContext";
+import { useSelector } from "react-redux";
 
 const TabletContainer = () => {
-  return <div>TabletContainer</div>;
+  const { lang } = useLang();
+  const languageData = useSelector((state) => state.localization);
+
+  return (
+    <div className={styles.tabletContainer}>
+      <div className={styles.tabletHeader}>
+        <h2>
+          {" "}
+          {languageData[lang]?.data?.home.telephone.meduim.title}
+          <br />
+          {languageData[lang]?.data?.home.telephone.meduim.restTitle}
+        </h2>
+      </div>{" "}
+      <div className={styles.tabletImg}>
+        <img src={tabletTelephone} alt="tabletTelphone" />
+      </div>{" "}
+      <div className={styles.tabletparaf}>
+        <p className="subtitleLight">
+          {languageData[lang]?.data?.home.telephone.paragraph}
+        </p>
+      </div>
+      <StoreButton height="48px" />
+    </div>
+  );
 };
 
 export default TabletContainer;
--- src/components/Telephone/TelephoneContainer/index.js
+++ src/components/Telephone/TelephoneContainer/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styles from "./styles.module.scss";
+import smallTelphone from "../../../assets/telephoneImg/smallTelephone.png";
+import StoreButton from "../../StoreButton";
+import { useLang } from "../../../context/LocalizationContext";
+import { useSelector } from "react-redux";
 
 const TelephoneContainer = () => {
-  return <div>TelephoneContainer</div>;
+  const { lang } = useLang();
+  const languageData = useSelector((state) => state.localization);
+
+  return (
+    <div className={styles.smallContainer}>
+      <div className={styles.smallHeader}>
+        <h3>
+          {languageData[lang]?.data?.home.telephone.meduim.title} <br />
+          {languageData[lang]?.data?.home.telephone.meduim.restTitle}
+        </h3>
+      </div>
+      <div className={styles.smallImg}>
+        <img src={smallTelphone} alt="smallTelphone" />
+      </div>
+      <div className={styles.smallparaf}>
+        <p className="subtitleLight">
+          {languageData[lang]?.data?.home.telephone.paragraph}
+        </p>
+      </div>
+      <StoreButton height="40px" />
+    </div>
+  );
 };
 
 export default TelephoneContainer;

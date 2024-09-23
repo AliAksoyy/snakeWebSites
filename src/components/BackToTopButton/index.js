54cc1124ec82c5dff1dee75e1fe9054f44bec2cf
AliAksoyy <alifeyza4017@gmail.com>
2024-09-24 02:11:39 +0300

    added

--- src/components/BackToTopButton/index.js
+++ src/components/BackToTopButton/index.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import styles from "./styles.module.scss";
 
 const BackToTopButton = () => {
-  return <div>BackToTopButton</div>;
+  const backToTopRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        backToTopRef.current.classList.add(styles.show);
+        backToTopRef.current.classList.remove(styles.hide);
+      } else {
+        backToTopRef.current.classList.remove(styles.show);
+        backToTopRef.current.classList.add(styles.hide);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`${styles.backTop} ${styles.hide}`}
+      ref={backToTopRef}
+    >
+      <svg
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="40" height="40" rx="8" fill="#41AEEF" />
+        <g clipPath="url(#clip0_904_15152)">
+          <path
+            d="M15 19L20 14M20 14L25 19M20 14V26"
+            stroke="white"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_904_15152">
+            <rect
+              width="16"
+              height="16"
+              fill="white"
+              transform="translate(12 12)"
+            />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
 };
 
 export default BackToTopButton;
--- src/components/MarkDownListContent/MarkDownContent.js
+++ src/components/MarkDownListContent/MarkDownContent.js
@@ -10,8 +10,80 @@ const MarkDownContent = ({ id, setLists }) => {
 
   const navigate = useNavigate();
 
-  useEffect(() => {});
-  return <div>MarkDownContent</div>;
+  useEffect(() => {
+    import(`../../markdown/${id}.${lang}.md`)
+      .then((res) => {
+        return fetch(res.default)
+          .then((res) => res.text())
+          .then((data) => setPost(data))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => {
+        console.log(err);
+        return navigate("*");
+      });
+  }, [id, navigate, lang]);
+
+  useEffect(() => {
+    const sideBarListsContent = Array.from(document.querySelectorAll("h4"));
+    console.log(sideBarListsContent);
+    let listsArray = sideBarListsContent?.reduce((acc, cur, index) => {
+      const list = {};
+      list.id = index + 1;
+      list.href = `#${cur.id}`;
+      list.text = cur.innerText;
+      acc.push(list);
+      return acc;
+    }, []);
+    setLists(listsArray);
+  }, [post, setLists]);
+  return (
+    <div className={styles.markDownContent}>
+      <>
+        <Markdown
+          options={{
+            forceBlock: true,
+            wrapper: "section",
+            overrides: {
+              p: {
+                props: {
+                  className: `${styles.paragraf} paragrafLight`,
+                },
+              },
+
+              a: {
+                props: {
+                  className: styles.link,
+                },
+              },
+              h4: {
+                props: {
+                  className: styles.title,
+                },
+              },
+              h6: {
+                props: {
+                  className: styles.subTitle,
+                },
+              },
+              li: {
+                props: {
+                  className: "paragrafLight",
+                },
+              },
+              table: {
+                props: {
+                  className: styles.table,
+                },
+              },
+            },
+          }}
+        >
+          {post}
+        </Markdown>
+      </>
+    </div>
+  );
 };
 
 export default MarkDownContent;
